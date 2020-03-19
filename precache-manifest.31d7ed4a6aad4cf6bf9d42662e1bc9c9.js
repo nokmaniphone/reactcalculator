97a2d1a3851b227f488af7d2307cf3b433c0aaa7
nokmaniphone <nokmaniphonesayavong@gmail.com>
2020-03-19 16:15:01 -0700

    Updates

---
+++ precache-manifest.31d7ed4a6aad4cf6bf9d42662e1bc9c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95dbc49d98789e7e47744eea50e8af6d",
+    "url": "/nokmaniphone/reactcalculator/index.html"
+  },
+  {
+    "revision": "5dc575eacedb6cc3eab7",
+    "url": "/nokmaniphone/reactcalculator/static/css/main.c265f7d8.chunk.css"
+  },
+  {
+    "revision": "5c38a5cfd05da9c6197b",
+    "url": "/nokmaniphone/reactcalculator/static/js/2.25a0e13f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nokmaniphone/reactcalculator/static/js/2.25a0e13f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dc575eacedb6cc3eab7",
+    "url": "/nokmaniphone/reactcalculator/static/js/main.8112e5af.chunk.js"
+  },
+  {
+    "revision": "0a6039d36422e1b227ce",
+    "url": "/nokmaniphone/reactcalculator/static/js/runtime-main.3ab3b346.js"
+  }
+]);
